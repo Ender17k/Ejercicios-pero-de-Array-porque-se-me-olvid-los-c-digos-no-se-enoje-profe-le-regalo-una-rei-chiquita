448fd52a1f38ffd5491d1d18b623d01328dfa680
Ender17k <alejandro.17est@gmail.com>
2025-09-14 19:24:42 -0600

    Delete ejercicio2.js

--- ejercicio2.js
+++
@@ -1,8 +0,0 @@
-// Ejercicio 2: Acceso a los elementos de un arreglo (orden y reversa)
-// Resultado esperado al ejecutar: [ 90, 80, 60, 45, 30, 20 ]
-
-const objetosMagicos = [20, 60, 45, 80, 90, 30];
-objetosMagicos.sort();
-objetosMagicos.reverse();
-console.log(objetosMagicos);
-// Resultado en consola: [ 90, 80, 60, 45, 30, 20 ]
