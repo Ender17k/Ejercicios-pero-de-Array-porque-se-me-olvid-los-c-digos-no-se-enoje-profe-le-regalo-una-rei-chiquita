2b7599f72e257998bc4e5571cbed39467400dba0
Ender17k <alejandro.17est@gmail.com>
2025-09-14 19:24:07 -0600

    Delete ejercicio6.js

--- ejercicio6.js
+++
@@ -1,9 +0,0 @@
-// Ejercicio 6: Agregar elementos al principio del arreglo (en el PDF se utilizó map para multiplicar)
-// Resultado esperado al ejecutar: [ 15, 30, 45, 60, 75 ]
-
-const anillosProteccion = [10, 20, 30, 40, 50];
-let anillosProteccionmult = anillosProteccion.map(function(anillo) {
-  return anillo * 1.5;
-});
-console.log(anillosProteccionmult);
-// Resultado en consola: [ 15, 30, 45, 60, 75 ]
