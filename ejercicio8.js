be266f323cefd9b45da4016075d9ec9473611e48
Ender17k <alejandro.17est@gmail.com>
2025-09-14 19:23:39 -0600

    Delete ejercicio8.js

--- ejercicio8.js
+++
@@ -1,9 +0,0 @@
-// Ejercicio 8: Uso de splice() para modificar el arreglo (en el PDF se usó some para verificar >100)
-// Resultado esperado al ejecutar: true
-
-const hechizos = [60, 80, 110, 95, 85];
-let siono = hechizos.some(function(hechizos) {
-  return (hechizos > 100);
-});
-console.log(siono);
-// Resultado en consola: true
